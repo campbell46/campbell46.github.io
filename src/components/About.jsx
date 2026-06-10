a800167602cb405b3d12b1f1f1669bedfc0b8422
Sarah Campbell <sarahcampbell4646@gmail.com>
2026-06-10 15:23:50 -0400

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,13 +27,13 @@ const About = () => {
             viewport={{once: false, amount: 0.3 }}
             className='flex-1'>
             <h2 className='h2 text-accent  drop-shadow-[2px_4px_4px_rgba(184,9,195,1)]'>About me.</h2>
-            <h3 className='font-bold text-3xl mb-4 drop-shadow tracking-wide'>
+            <h3 className='font-bold text-3xl mb-4 tracking-wide'>
               My name is Sarah, I currently live in Rockland, ON.
             </h3>
-            <p className='mb-2 drop-shadow'>
+            <p className='mb-2'>
               I've had a passion for tech since a young age, always wondering how things worked. I took a programming course in highschool and it changed the way I view everything.
             </p>
-            <p className='mb-6 drop-shadow'>
+            <p className='mb-6'>
               Before settling down in Rockland and completing the Lighthouse Labs Web Dev Bootcamp, I was previously working as a conductor on the railway up North and prior to that I was a vegetation technician all over the West side of Canada. Finally pursuing my passion of programming and excited to see where it takes me.
             </p>
             {/* skills */}
